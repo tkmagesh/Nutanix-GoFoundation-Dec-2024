d58bae8bb893006e2d0344f86a3476d1b92aee6e
Magesh Kuppan <tkmagesh77@gmail.com>
2024-12-04 22:54:09 +0530

    assigments

---
+++ 06-collections/05-assignment.go
@@ -0,0 +1,8 @@
+/*
+Accept a range from the user and print all the prime numbers between the given range
+*/
+/*
+Write the logic for generating prime nos in a "generatePrimes" function
+Print the generated prime numbers in the main() function
+*/
+
---
+++ 06-collections/06-assignment.go
@@ -0,0 +1,13 @@
+/*
+Find the most occurring "word size" in the given string and print the most occuring "word size" & the number of occurances
+Use "strings.Split()" to get the words
+*/
+/*
+	ex output:
+	5 letter words occurs the most with 31 occurances
+*/
+package main
+
+func main() {
+	str := "Commodo Lorem enim dolore culpa minim ipsum ex excepteur in Commodo duis nulla ex laborum irure sunt incididunt Incididunt amet Lorem amet dolor sit consectetur culpa esse quis laborum pariatur laborum fugiat mollit Mollit voluptate aliquip Lorem incididunt mollit pariatur eu enim proident culpa esse laborum voluptate Nostrud aliqua magna ipsum qui duis euNisi pariatur sit do magna Lorem nostrud voluptate occaecat occaecat quis dolore irure Velit aliqua reprehenderit eu duis aliqua excepteur duis non enim Nostrud qui voluptate enim eiusmod dolor proident laboris nostrud commodo laborum aliquip sunt Exercitation do anim do ullamco Ipsum eiusmod aute qui ea consectetur Veniam laborum occaecat mollit pariatur commodo id ullamco dolore ipsum sit dolore elit fugiatUt ad aliqua dolor quis velit reprehenderit Dolore aliquip exercitation do fugiat Pariatur irure aliqua magna quis"
+}
